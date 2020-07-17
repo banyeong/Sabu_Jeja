bbcfb14adbd43fb6f77f60ef282ec0e9b27306d8
정수빈 <banyeong321@gmail.com>
2020-07-17 13:59:55 +0900

    story2_2
    스토리 2 선택지 대화 후의 대화까지 씬 작성함. 씬 연결 필요.

--- Assets/C#/STORY/2/story_2_2.cs
+++ Assets/C#/STORY/2/story_2_2.cs
@@ -5,39 +5,25 @@ using UnityEngine.UI;
 using UnityEngine.UIElements;
 using UnityEditor;
 
-#pragma warning disable 0649
-public class story_2_choice1 : MonoBehaviour
+public class story_2_2 : MonoBehaviour
 {
+#pragma warning disable 0649
+
     [SerializeField] private SpriteRenderer sprite_Charcter; //캐릭터
     [SerializeField] private SpriteRenderer sprite_dialogue; //대화창
     [SerializeField] private Text txt_dialogue; //대화내용
     [SerializeField] private SpriteRenderer sprite_name; //누가 말하는지 표시
     [SerializeField] private SpriteRenderer sprite_background; //배경
 
-    [SerializeField] private UnityEngine.UI.Button choice_1; // 선택지 버튼1
-    [SerializeField] private UnityEngine.UI.Button choice_2; // 선택지 버튼2
-
-    private bool isShow = false;
+    private bool isFinal = true; // 스토리 2의 마지막 다이얼로그인가?
     private int count = 0;
 
     [SerializeField] private Dialogue[] dialogue;
 
-    public void ShowButton() //선택지 1을 눌렀을 때 나오는 UI들
+    public void FinalDialogue() //대화활성화
     {
-        choice_1.gameObject.SetActive(false);
-        choice_2.gameObject.SetActive(false);
-        sprite_dialogue.gameObject.SetActive(true);
         sprite_name.gameObject.SetActive(true);
-        txt_dialogue.gameObject.SetActive(true);
 
-        isShow = true;
-        count = 0;
-
-        NextDialogue();
-    }
-
-    public void NextDialogue() //대화활성화
-    {
         txt_dialogue.text = dialogue[count].dialogue;
         sprite_Charcter.sprite = dialogue[count].cg;
         sprite_name.sprite = dialogue[count].nickname;
@@ -48,17 +34,17 @@ public class story_2_choice1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isShow == true)
+        if (isFinal == true)
         {
             if (Input.GetMouseButtonDown(0))
             {
                 if (count < dialogue.Length)
                 {
-                    NextDialogue();
+                    FinalDialogue();
                 }
                 else
                 {
-                    //씬전환
+                    
                 }
             }
         }
