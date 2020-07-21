dec7fc5ba7964393d1ad8e01763658a51f9b934a
정수빈 <banyeong321@gmail.com>
2020-07-21 13:24:08 +0900

    Main & BTN
    메인 화면 구성 및 수련일정과 뒤로가기 구현

---
+++ Assets/C#/LoadScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public void LoadTraining()
+    {
+        SceneManager.LoadScene(13);
+    }
+    public void LoadMain()
+    {
+        SceneManager.LoadScene(14);
+    }
+}
