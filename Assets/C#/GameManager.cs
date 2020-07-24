0b8cc168eb82fbbbcc588c206fbf2c64a91ea96f
정수빈 <banyeong321@gmail.com>
2020-07-24 14:36:53 +0900

    Date
    날짜 변경 필요

--- Assets/C#/GameManager.cs
+++ Assets/C#/GameManager.cs
@@ -1,13 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; set; }
 
-    
     public StudentStat stat { get; set; }
+
+    public Text DateText; // 메인화면 날짜 텍스트
+    public int Year;
+    public int Month;
+
+    //날짜 치환 함수 필요할 것 같음
+    public void DateChange()
+    {
+        
+    }
 
     private void Awake()
     {
@@ -22,5 +32,10 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         stat = new StudentStat() { weeks = 12, currentweeks = 1, _currentMslStr = 100, _currentMoralStr = 100, _currentWealth = 100, _cureentFavorability = 100 };
+    }
+
+    private void Update()
+    {
+        DateText.text = GameManager.Instance.stat.currentweeks + "주"; //몇 년 몇 개월 몇 주로 작성할 필요 있음
     }
 }
