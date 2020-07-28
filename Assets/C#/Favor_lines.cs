399800010abb64e1d8e7088e25267d41e39dbd94
정수빈 <banyeong321@gmail.com>
2020-07-28 16:17:36 +0900

    Favor_lines_1
    호감도 대사 구현 일단 스크립트만 만들어봄

---
+++ Assets/C#/Favor_lines.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Favor_lines : MonoBehaviour
+{
+    public Text faver_lines;
+
+    public string A = "어서오세요, 스승님";
+    public string B = "식사시간 입니다.";
+
+    public void one()
+    {
+        if (GameManager.Instance.stat.CurrentFavorability < 100)
+        {
+            //faver_lines.text = A || B;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
