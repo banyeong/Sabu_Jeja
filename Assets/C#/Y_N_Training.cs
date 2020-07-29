e4cc15b2ba9ee6f7ebd8abeeb07943463940ea33
정수빈 <banyeong321@gmail.com>
2020-07-29 15:41:42 +0900

    POP-UP
    봉술 단련 팝업창 띄우기 완료

---
+++ Assets/C#/Y_N_Training.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+
+public class Y_N_Training : MonoBehaviour
+{
+    [SerializeField] private GameObject Big;
+    [SerializeField] private SpriteRenderer POPUP;
+    [SerializeField] private Text TRname;
+    [SerializeField] private Text Explain;
+
+    [SerializeField] private UnityEngine.UI.Button Yes;
+    [SerializeField] private UnityEngine.UI.Button No;
+
+    [SerializeField] private Dialogue[] trname;
+    [SerializeField] private Dialogue[] explain;
+    private int count = 0;
+
+    public void showPOPUP()
+    {
+        Big.gameObject.SetActive(true);
+        TRname.text = trname[count].dialogue;
+        Explain.text = explain[count].dialogue;
+    }
+
+    public void hidePOPUP()
+    {
+        Big.gameObject.SetActive(false);
+    }
+}
