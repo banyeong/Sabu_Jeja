30f51cd77479913ca5f17591a246a57b0deea87d
정수빈 <banyeong321@gmail.com>
2020-08-03 15:48:25 +0900

    End Story
    비호감도 및 호감도 엔딩 스토리 씬 완료. 조건은 아직임.

---
+++ Assets/C#/STORY/End_Story/Happy_End_2.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Happy_End_2 : MonoBehaviour
+{
+#pragma warning disable 0649
+
+    [SerializeField] private SpriteRenderer sprite_Charcter; //캐릭터
+    [SerializeField] private SpriteRenderer sprite_dialogue; //대화창
+    [SerializeField] private Text txt_dialogue; //대화내용
+    [SerializeField] private SpriteRenderer sprite_name; //누가 말하는지 표시
+    [SerializeField] private SpriteRenderer sprite_background; //배경
+
+    private bool isDialogue = true;
+    private int count = 0;
+
+    [SerializeField] private Dialogue[] dialogue;
+
+    public void NextDialogue() //대화활성화
+    {
+        sprite_name.gameObject.SetActive(true);
+        sprite_Charcter.gameObject.SetActive(true);
+        txt_dialogue.text = dialogue[count].dialogue;
+        sprite_Charcter.sprite = dialogue[count].cg;
+        sprite_name.sprite = dialogue[count].nickname;
+        sprite_background.sprite = dialogue[count].bg;
+        count++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDialogue == true)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (count < dialogue.Length)
+                {
+                    NextDialogue();
+                }
+                else
+                {
+                    //나중에 바꿔야 함
+                    SceneManager.LoadScene(15);
+                }
+            }
+        }
+    }
+}
