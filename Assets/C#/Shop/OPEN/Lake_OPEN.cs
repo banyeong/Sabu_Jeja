be2031abec064448bef5f571b05910250fb515aa
정수빈 <banyeong321@gmail.com>
2020-08-09 15:47:15 +0900

    Go Out
    외출 시스템 거의 구현 완료. 음식점, 호수, 산 - 한 번 갔으면 3개월 후에 갈 수 있다는 조건은 아직 설정 X 클릭했을 때 랜덤으로 스토리 진행되는 것만 가능

---
+++ Assets/C#/Shop/OPEN/Lake_OPEN.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Lake_OPEN : MonoBehaviour
+{
+    public void RayShop()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = -10;
+        RaycastHit2D hit2D = Physics2D.Raycast(mousePos, transform.forward, 30);
+
+        if (hit2D.collider != null)
+        {
+            if (hit2D.collider.CompareTag("Lake"))
+            {
+                RandomScene();
+            }
+        }
+    }
+
+    public void RandomScene()
+    {
+        int x = Random.Range(0, 3);
+
+        switch (x)
+        {
+            case 0:
+                SceneManager.LoadScene(28);
+                break;
+            case 1:
+                SceneManager.LoadScene(29);
+                break;
+            case 2:
+                SceneManager.LoadScene(30);
+                break;
+        }
+    }
+
+    //스타트 및 업데이트 함수
+    private void Start()
+    {
+        //closeShop.onClick.AddListener(DeActiveGAShop);
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RayShop();
+        }
+    }
+}
